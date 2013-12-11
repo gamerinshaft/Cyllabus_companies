afd6750ccd17261d45414e2dabb351e7b84bd82e
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-12-11 18:03:32 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cyllabus::Application.config.secret_token = '8b67d3ca69765a0bfacd607dc744ac96b7f351225ddb176bdef2668a9ec69452e7c5d21990269fbd09bccb94a5538d56cd098c33fcccd39d81dceb96e0c2f64b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Cyllabus::Application.config.session_store :cookie_store, key: '_cyllabus_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Cyllabus::Application.config.session_store :active_record_store
