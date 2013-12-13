6e036a6f78a2a65d20d3699dfdc205ecf2c434b0
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-12-14 06:15:15 +0900

    add column

---
+++ db/migrate/20131213211451_add_latitude_longitude_gmaps_to_companies.rb
@@ -0,0 +1,7 @@
+class AddLatitudeLongitudeGmapsToCompanies < ActiveRecord::Migration
+  def change
+    add_column :companies, :latitude, :float
+    add_column :companies, :longitude, :float
+    add_column :companies, :gmaps, :boolean
+  end
+end
